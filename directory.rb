d7209e4a51a068259dff4588f0688443b76e55de
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2017-06-21 19:05:50 +0100

    Introduced methods for greater readability and flexibility.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,4 @@
 # Let's put all students into an array.
-
 students = [
   "Dr. Hannibal Lecter",
   "Darth Vader",
@@ -14,11 +13,21 @@ students = [
   "Norman Bates"
 ]
 
-puts "The Students of Villains Academy"
-puts "--------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The Studends of Villains Academy"
+  puts "--------------"
 end
 
-# Here we print the total number of the students.
-puts "Overall we have #{students.count} great students."
+def print_list(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students."
+end
+
+print_header
+print_list(students)
+print_footer(students)
