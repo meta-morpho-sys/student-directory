c8d147ac9200f113e7ba89cae3ec15ccaf08e6b6
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2017-06-22 14:58:44 +0100

    prints a number before the name of each student

---
+++ directory_numbers.rb
@@ -0,0 +1,40 @@
+  puts "Please enter the names of the students"
+  puts "To finish just hit return twice"
+
+  #create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+
+  #while the name is not empty repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from user. If no, a contuinuous while loop will be triggered
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+def print_header
+  puts "The Studends of Villains Academy"
+  puts "--------------"
+end
+
+def print_list(students)
+  students.each_with_index do |student, i|
+    puts "#{i+1} #{student[:name]} (#{student[:cohort]} cohort)"  #prints a number before the name of each student.
+    def input_students
+  end
+end
+
+def print_footer(students)
+  puts "Overall we have #{students.count} great students."
+end
+
+students = input_students
+print_header
+print_list(students)
+print_footer(students)
